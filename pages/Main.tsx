b1bc56b929317742d91d701bf61f40925aea2709
s-yuma <yuma.sueishi@gmail.com>
2023-02-23 22:21:15 +0900

    kousinn

---
+++ pages/Main.tsx
@@ -0,0 +1,58 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.scss'
+import Link from "next/link"
+// import HomeCalender from '../components/home/HomeCalender';
+import dynamic from 'next/dynamic';
+import Header from '../components/header/HomeHeader';
+import HomeHeader from '../components/header/HomeHeader';
+import style from "../components//home//HomeCalender.module.scss"
+
+
+const HomeCalender = dynamic(() => import('../components/home/HomeCalender'), {
+    ssr: false
+});
+
+const Main: NextPage = (props) => {
+  const users =[
+    {name:"母", style1:styles.btn_m1, style2:styles.btn_m2},
+    {name:"父", style1:styles.btn_f1, style2:styles.btn_f2},
+    {name:"息子", style1:styles.btn_b1, style2:styles.btn_b2},
+    {name:"娘", style1:styles.btn_s1, style2:styles.btn_s2},
+    {name:"祖母", style1:styles.btn_gm1, style2:styles.btn_gm2},
+    {name:"祖父", style1:styles.btn_gf1, style2:styles.btn_gf2},
+  ];
+
+  return (
+    <>
+    <Header/>
+    <div className={styles.body}>
+      <div className={styles.container}>
+
+
+     
+      {users.map((user) => {
+        return (
+          
+          <div className={`${user.style1} ${styles.slide}`} key={user.name}>
+            <Link href={{ pathname: `/record/Record[name]`, query: {name: user.name} }}  className={`${user.style2} ${styles.slide}`} >
+              {user.name}
+            </Link>
+          </div>
+      
+          
+        );
+        
+      })}
+        
+
+      </div>
+      <div className={style.container}>
+        <HomeCalender />
+      </div>
+      </div>
+    </>
+        
+  )
+}
+
+export default Main
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,51 +5,52 @@ import Link from "next/link"
 import dynamic from 'next/dynamic';
 import Header from '../components/header/HomeHeader';
 import HomeHeader from '../components/header/HomeHeader';
-import style from "../components//home//HomeCalender.module.scss"
-
-
-const HomeCalender = dynamic(() => import('../components/home/HomeCalender'), {
-    ssr: false
-});
+import style from "../styles//Index.module.scss"
+import { Accordion } from 'react-bootstrap-accordion'
+import { AccordionDetails } from '@mui/material';
+import Image from 'next/image';
 
 const Home: NextPage = (props) => {
-  const users =[
-    {name:"母", style1:styles.btn_m1, style2:styles.btn_m2},
-    {name:"父", style1:styles.btn_f1, style2:styles.btn_f2},
-    {name:"息子", style1:styles.btn_b1, style2:styles.btn_b2},
-    {name:"娘", style1:styles.btn_s1, style2:styles.btn_s2},
-    {name:"祖母", style1:styles.btn_gm1, style2:styles.btn_gm2},
-    {name:"祖父", style1:styles.btn_gf1, style2:styles.btn_gf2},
-  ];
+
 
   return (
     <>
-    <Header/>
-    <div className={styles.body}>
-      <div className={styles.container}>
+      <Header/>
+      <h1 className={style.title}>家庭内医療管理アプリ</h1>
 
-
-     
-      {users.map((user) => {
-        return (
-          
-          <div className={`${user.style1} ${styles.slide}`} key={user.name}>
-            <Link href={{ pathname: `/record/Record[name]`, query: {name: user.name} }}  className={`${user.style2} ${styles.slide}`} >
-              {user.name}
-            </Link>
-          </div>
-      
-          
-        );
-        
-      })}
-        
-
-      </div>
       <div className={style.container}>
-        <HomeCalender />
-      </div>
-      </div>
+      <Image className={style.img} src="/images/home1.png" width={400} height={400} alt="画像" ></Image>
+      <Link href="/Main" id="button" >
+        <button className={style.button}>ホームへ</button>
+      </Link>
+      <details className={style.details}>
+          <summary>使い方</summary>
+          <details>
+            <summary>ホーム</summary>
+            <div>横スクロールをしてユーザーを選択してください。</div>
+            <div>カレンダーでは全ユーザーのスケジュールを確認できます。</div>
+          </details>
+          <details>
+            <summary>予約</summary>
+            <div>病院の予約日時などを入力して登録してください。</div>
+            <div>カレンダーでは個人のスケジュールを確認できます。</div>
+          </details>
+          <details>
+            <summary>記録</summary>
+            <div>（💩）便や（💧）尿をした時にボタンを押下する事で日時を記録することができます。</div>
+            <div>PCでは「Excel出力」ボタンを押下する事で記録したデータを出力できます。</div>
+          </details>
+          <details>
+            <summary>薬歴</summary>
+            <div>薬名と病名・症状を入力して登録してください。</div>
+            <div>薬歴リストでは表示が白色が服用中、灰色が服用が終わったもので管理します。</div>
+            <div>服用をやめた薬は×アイコンを押下して</div>
+            <div>PCでは「Excel出力」ボタンを押下する事で記録したデータを出力できます。</div>
+          </details>
+      </details>
+        </div>
+
+      
     </>
         
   )
