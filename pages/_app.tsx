a1c23d409868cef67eb40c0a521a6874b15a7b8b
s-yuma <yuma.sueishi@gmail.com>
2023-02-23 22:43:06 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,15 +8,6 @@ import Head from "next/head"
   return (
     <>
         <Head>
-        <link
-href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-crossOrigin="anonymous" 
-/>
-<script
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-crossOrigin="anonymous"></script>
           <meta name="viewport" content="width=device-width, user-scale=yes,
           initial-scale=1.0, maximum-scale=5.0" />
         <title>テストページ</title>
